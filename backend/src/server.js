9e7a25fc328f5560b5fba72773cb783fdf98152e
Sílvio Rodrigues Azevedo <silvioclipper@gmail.com>
2019-08-19 20:55:23 -0300

    Controller completo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes')
+const rota = require('./routes')
 
 const server = express()
 
 mongoose.connect('mongodb+srv://silvioclipper:Y0k02015@cluster0-7lncu.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser:true
 })
-server.use(express.json)
-server.use(routes)
+server.use(express.json())
+server.use(rota)
 
 //server.set('port', process.env.PORT || 3333)
 
